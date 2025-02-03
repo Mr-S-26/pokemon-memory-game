f990c7db4428ddd16a6fc83341760190d55985c6
Mr-S-26 <michaelryansia26@gmail.com>
2025-02-03 21:55:28 +0800

    fix base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pokemon-memory-game/',
+  base: './',
   define: {
     __APP_TITLE__: JSON.stringify('Pokemon Memory Game'),
   },
